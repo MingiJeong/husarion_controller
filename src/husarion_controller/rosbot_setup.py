7edff8d54b92a0149f7b2ca893d6d52f22e71b91
Lab Computer <dartmouthrobotics@lab_notebook.com>
2021-10-26 17:48:16 -0400

    abs turning implemented

--- src/husarion_controller/rosbot_setup.py
+++ src/husarion_controller/rosbot_setup.py
@@ -2,8 +2,11 @@
 
 # import relevant libraries
 import rospy
+
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
 
 # import python modules
 import numpy as np
@@ -23,12 +26,32 @@ class ROSBOT():
 
         """ Constructor """
         self.rate = rospy.Rate(FREQUENCY)
+        self.heading = None
 
         # publisher and subscriber
         self._cmd_pub = rospy.Publisher(DEFAULT_CMD_VEL, Twist, queue_size=1)
-        # self._laser_sub =rospy.Subscriber(DEFAULT_SCAN, LaserScan, self._laser_callback, queue_size=1)
+
+        self._odom_sub = rospy.Subscriber(DEFAULT_ODOM, Odometry, self._odom_callback, queue_size=1)
+        self._laser_sub = rospy.Subscriber(DEFAULT_SCAN, LaserScan, self._laser_callback, queue_size=1)
         self.lin_vel = LIN_VEL
         self.ang_vel = ANG_VEL
+
+
+    def _laser_callback(self, laser_msg):
+        pass
+
+    
+    def _odom_callback(self, odom_msg):
+        # heading update
+        self.heading = self.get_heading(odom_msg)
+
+
+    def get_heading(odom_msg):
+        orientation_q = odom_msg.pose.pose.orientation
+        orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+        (roll,pitch,yaw) = euler_from_quaternion(orientation_list)
+
+        return yaw
 
 
     def move(self, lin_vel, ang_vel):
@@ -96,6 +119,35 @@ class ROSBOT():
         self.stop()
 
 
+    def rotate_abs(self, dest_angle):
+        """
+        Rotate in place the robot of rotation_angle (rad) based on fixed velocity.
+        depending on the rotation direction (sign of angle)
+        """
+
+        twist_msg = Twist()
+        angle_to_go = dest_angle - self.heading
+
+        if angle_to_go >= 0: # counter-clockwise True
+            twist_msg.angular.z = self.ang_vel
+        else: # counter-clockwise False, i.e., clockwise
+            twist_msg.angular.z = - self.ang_vel
+
+        start_time = rospy.get_rostime()
+        duration = rospy.Duration(abs(angle_to_go) / self.ang_vel)
+
+        while not rospy.is_shutdown():
+            if rospy.get_rostime() - start_time >= duration:
+                break
+
+            else:
+                self._cmd_pub.publish(twist_msg)
+                self.rate.sleep()
+        
+        # stopper
+        self.stop()
+
+
     def spin(self):
         # while not rospy.is_shutdown():
         try: 
@@ -108,7 +160,12 @@ class ROSBOT():
             self.rotate_rel(- math.pi/6)
             rospy.loginfo("rotation 2 finish")
 
+
+            self.rotate_abs(math.pi * 3/4)
+            rospy.loginfo("rotation abs finish")
+
             # self.rate.sleep()
+
 
         except rospy.ROSInterruptException:
             rospy.logerr("ROS node interrupted.")
