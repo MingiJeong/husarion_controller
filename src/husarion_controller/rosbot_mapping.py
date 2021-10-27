38809a46e6a695b9eabe5671f275db88fa90026b
Lab Computer <dartmouthrobotics@lab_notebook.com>
2021-10-26 23:26:20 -0400

    map tf

--- src/husarion_controller/rosbot_mapping.py
+++ src/husarion_controller/rosbot_mapping.py
@@ -10,17 +10,23 @@
 # import relevant libraries
 from husarion_controller.rosbot_setup import DEFAULT_CMD_VEL
 import rospy
+import tf
+import tf2_ros
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import OccupancyGrid
+import geometry_msgs.msg
 
 # import python modules
 import numpy as np
 import math
+import sys
 
 # constants
 MAP_FREQ = 10 # Hz
 
 DEFAULT_MAP_TOPIC = "map"
+DEFAULT_MAP_FRAME = "map"
+DEFAULT_ODOM_FRAME = "odom"
 DEFAULT_SCAN = 'scan'
 
 
@@ -36,18 +42,42 @@ class Mapper():
         self.resolution = rospy.get_param("map_resolution", 0.05) # m/cell
         self.width = rospy.get_param("width", 100) # cell
         self.height = rospy.get_param("hegiht", 100) # cell
+        self.transform_data = (- self.resolution * self.width // 2, 
+                               - self.resolution * self.height // 2,
+                               0, 0, 0)
 
 
     def _laser_callback(self, laser_msg):
         # do something for mapping
         self.build_map()
 
+    def static_broadcaster(self):
+        broadcaster = tf2_ros.StaticTransformBroadcaster()
+        static_transformStamped = geometry_msgs.msg.TransformStamped()
+
+        static_transformStamped.header.stamp = rospy.Time.now()
+        static_transformStamped.header.frame_id = DEFAULT_ODOM_FRAME
+        static_transformStamped.child_frame_id = DEFAULT_MAP_FRAME
+
+        static_transformStamped.transform.translation.x = self.transform_data[0]
+        static_transformStamped.transform.translation.y = self.transform_data[1]
+        static_transformStamped.transform.translation.z = 0
+
+        quat = tf.transformations.quaternion_from_euler(
+                   self.transform_data[2],self.transform_data[3],self.transform_data[4])
+        static_transformStamped.transform.rotation.x = quat[0]
+        static_transformStamped.transform.rotation.y = quat[1]
+        static_transformStamped.transform.rotation.z = quat[2]
+        static_transformStamped.transform.rotation.w = quat[3]
+
+        broadcaster.sendTransform(static_transformStamped)
+
 
     def build_map(self):
         map_msg = OccupancyGrid()
 
         # header
-        map_msg.header.frame_id = "map"
+        map_msg.header.frame_id = DEFAULT_MAP_FRAME
         map_msg.header.stamp = rospy.Time.now()
 
         # info
@@ -64,5 +94,11 @@ class Mapper():
 
         _grid_2d = np.reshape(_grid_1d, (self.height, self.width))
 
+
+        map_msg.data = _grid_1d.astype(np.int8) # grid explicit type casting
+
+
+        # publishing
         self._map_pub.publish(map_msg)
+        self.static_broadcaster()
         self.rate.sleep()
