c5a6141e5dceef1ea51127550a20e429760834b6
Lab Computer <dartmouthrobotics@lab_notebook.com>
2021-10-27 08:53:23 -0400

    scanner transformation

--- src/husarion_controller/rosbot_mapping.py
+++ src/husarion_controller/rosbot_mapping.py
@@ -20,14 +20,23 @@ import geometry_msgs.msg
 import numpy as np
 import math
 import sys
+from enum import Enum
 
 # constants
 MAP_FREQ = 10 # Hz
+TRANSFORM_DURATION = 3 # sec
 
 DEFAULT_MAP_TOPIC = "map"
+DEFAULT_SCAN_TOPIC = 'scan'
+
 DEFAULT_MAP_FRAME = "map"
 DEFAULT_ODOM_FRAME = "odom"
-DEFAULT_SCAN = 'scan'
+DEFAULT_LASER_FRAME = "laser"
+
+
+class MapLaserUpdate(Enum):
+    not_received = 0
+    received = 1
 
 
 class Mapper():
@@ -37,21 +46,149 @@ class Mapper():
 
         # publisher and subscriber
         self._map_pub = rospy.Publisher(DEFAULT_MAP_TOPIC, OccupancyGrid, queue_size=1)
-        self._laser_sub = rospy.Subscriber(DEFAULT_SCAN, LaserScan, self._laser_callback, queue_size=1)
+        self._laser_sub = rospy.Subscriber(DEFAULT_SCAN_TOPIC, LaserScan, self._laser_callback, queue_size=1)
+        self._tf_listener = tf.TransformListener()
 
+        # static data of the map
+        self.map_origin_x = 0
+        self.map_origin_y = 0
         self.resolution = rospy.get_param("map_resolution", 0.05) # m/cell
-        self.width = rospy.get_param("width", 100) # cell
-        self.height = rospy.get_param("hegiht", 100) # cell
+        self.width = rospy.get_param("width", 100) # cell ==> max_col
+        self.height = rospy.get_param("hegiht", 100) # cell ==> max_row
         self.transform_data = (- self.resolution * self.width // 2, 
                                - self.resolution * self.height // 2,
-                               0, 0, 0)
+                               0, 0, 0)  # transformed translation (x,y), rotation (r,p,y)
+
+        self.map_max_x = self.width * self.resolution
+        self.map_max_y = self.width * self.resolution
+        self.map_min_x = self.map_origin_x
+        self.map_min_y = self.map_origin_y
+
+        # laser scanner data by ROSBOT
+        self.laser_ang_min = None # rad
+        self.laser_ang_max = None # rad
+        self.laser_ang_inc = None # rad
+        self.range_min = None # m
+        self.range_max = None # m
+        self.laser_data_update = MapLaserUpdate.received
+
+
+    def _update_laser_data(self):
+        """ initial update of the laser msg when booting up """
+        laser_msg = rospy.wait_for_message(DEFAULT_SCAN_TOPIC, LaserScan)
+        self.laser_ang_min = laser_msg.angle_min
+        self.laser_ang_max = laser_msg.angle_max
+        self.laser_angle_inc = laser_msg.angle_inc
+        self.range_min = laser_msg.range_min
+        self.range_max = laser_msg.range_max
+        self.laser_data_update = MapLaserUpdate.received
 
 
     def _laser_callback(self, laser_msg):
-        # do something for mapping
-        self.build_map()
+
+        # process laser data once we receive initial data
+        if self.laser_data_update == MapLaserUpdate.received:
+            
+            map_T_scan = self.get_transform_scan_to_map()
+            map_T_odom = self.get_transform_odom_to_map()
+
+            rospy.loginfo("transformation working well {} {}".format(map_T_scan, map_T_odom))
+
+            # do something for mapping
+            self.build_map()
+
+
+
+    def colrow_to_xy(self, j,i):
+        """
+        convert column and row index into x,y coordinate (m)
+        
+        Arguments:
+            j: column
+            i: row
+        """
+        x = j * self.resolution + self.map_origin_x
+        y = i * self.resolution + self.map_origin_y
+
+        return x,y
+
+
+    def xy_to_colrow(self, x,y):
+        """
+        convert x,y coordinate (m) into column and row index (int)
+        
+        Arguments:
+            x: coord_x (m)
+            y: coord_y (m)
+        """
+
+        i = int((y - self.map_origin_y) // self.resolution) # row
+        j = int((x - self.map_origin_x) // self.resolution) # col
+
+        return j, i
+
+    # TODO: convert laser scan data into map frame (i,j) transformation
+
+    def check_inside_map(self, i, j):
+        """
+        check the given col, row index is bounded by the maximum map area
+        """
+        if i >= 0 and j>= 0:
+            if j <= self.width and i <= self.height:
+                return True
+
+
+
+    def get_transform_scan_to_map(self):
+        """
+        obtain transformation matrix between laser and map
+        """
+        try:
+            self._tf_listener.waitForTransform(DEFAULT_MAP_FRAME,
+                                            DEFAULT_LASER_FRAME, 
+                                            rospy.Time(0), 
+                                            rospy.Duration(TRANSFORM_DURATION))
+
+            (trans, rot) = self._tf_listener.lookupTransform(DEFAULT_MAP_FRAME,
+                                                            DEFAULT_LASER_FRAME,
+                                                            rospy.Time(0))
+            t = tf.transformations.translation_matrix(trans)
+            R = tf.transformations.quaternion_matrix(rot) 
+
+            return np.dot(t, R)
+
+        except:
+            rospy.loginfo("transformation not working from %s to %s" 
+                            %(DEFAULT_LASER_FRAME, DEFAULT_MAP_FRAME))
+
+
+    def get_transform_odom_to_map(self):
+        """
+        obtain transformation matrix between odom and map
+        """
+        try:
+            self._tf_listener.waitForTransform(DEFAULT_MAP_FRAME,
+                                            DEFAULT_ODOM_FRAME, 
+                                            rospy.Time(0), 
+                                            rospy.Duration(TRANSFORM_DURATION))
+
+            (trans, rot) = self._tf_listener.lookupTransform(DEFAULT_MAP_FRAME,
+                                                            DEFAULT_ODOM_FRAME,
+                                                            rospy.Time(0))
+            t = tf.transformations.translation_matrix(trans)
+            R = tf.transformations.quaternion_matrix(rot) 
+
+            return np.dot(t, R)
+
+        except:
+            rospy.loginfo("transformation not working from %s to %s" 
+                            %(DEFAULT_ODOM_FRAME, DEFAULT_MAP_FRAME))
+
 
     def static_broadcaster(self):
+        """
+        static transform broadcaster between odom and map frame
+        """
         broadcaster = tf2_ros.StaticTransformBroadcaster()
         static_transformStamped = geometry_msgs.msg.TransformStamped()
 
@@ -74,6 +211,9 @@ class Mapper():
 
 
     def build_map(self):
+        """
+        building and filling the Occupancy grid map
+        """
         map_msg = OccupancyGrid()
 
         # header
@@ -86,8 +226,8 @@ class Mapper():
         map_msg.info.height = self.height
         map_msg.data = range(self.width * self.height)
 
-        map_msg.info.origin.position.x = 0
-        map_msg.info.origin.position.y = 0
+        map_msg.info.origin.position.x = self.map_origin_x
+        map_msg.info.origin.position.y = self.map_origin_y
 
         _grid_1d = np.zeros(len(map_msg.data))
         _grid_1d[:] = -1 # unknown
@@ -95,7 +235,8 @@ class Mapper():
         _grid_2d = np.reshape(_grid_1d, (self.height, self.width))
 
 
-        map_msg.data = _grid_1d.astype(np.int8) # grid explicit type casting
+        # TODO: grid change
+        map_msg.data = _grid_1d.astype(np.uint8) # grid explicit type casting
 
 
         # publishing
